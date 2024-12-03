eecd1cc892f526d192499e5d09468b652b83482e
Sameh Elkhyat <129992307+SamehElkhyat@users.noreply.github.com>
2024-12-03 17:25:01 +0200

    first commit

--- src/Components/Offers/Offers.jsx
+++ src/Components/Offers/Offers.jsx
@@ -1,11 +1,10 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import pizza1 from "../images/About Image/Double-Deal-Large.webp";
 import pizza2 from "../images/About Image/Double-Deal-Medium.webp";
 import pizza3 from "../images/About Image/Double-Deal-Small.webp";
 import pizza4 from "../images/About Image/Treat-Meal.webp";
 import pizza5 from "../images/About Image/Roma-Box-2-1024x384.webp";
 import pizza6 from "../images/About Image/Triple-Box_Banner-1024x384.webp";
-import '../Offers/Offers.css'
 import {
   Carousel,
   CarouselItem,
@@ -36,17 +35,18 @@ const items = [
     src: pizza4,
     altText: 'Corn',
     caption: 'Corn pizza is a unique twist on the classic pie, combining the sweet crunch of corn with the savory flavors of cheese and other toppings. Often paired with creamy mozzarella, tangy tomato sauce, and sometimes a hint of herbs like basil or oregano, corn adds a refreshing balance of texture and flavor. It\'s a popular choice in many parts of the world, especially in regions that love sweet-savory combinations. Whether baked in a traditional oven or cooked in a wood-fired one, corn pizza offers a delicious, satisfying experience for those looking to try something different from the usual pepperoni',
+    key: 4,
   },
   {
     src: pizza5,
     altText: 'Vegetable',
     caption: 'Vegetable pizza is a vibrant and healthy take on the classic favorite. This pizza combines the natural sweetness of corn with a variety of fresh vegetables like bell peppers, onions, tomatoes, mushrooms, and olives. Topped with creamy mozzarella and a flavorful tomato or pesto base, it offers a delightful mix of textures and flavors. Packed with nutrients and bursting with color, it\'s perfect for those seeking a wholesome, plant-forward meal without sacrificing taste. Whether enjoyed as a quick weeknight dinner or a party snack, corn and vegetable pizza is a crowd-pleaser thats both delicious and nutritious.',
-    key: 3,
+    key: 5,
   }, {
     src: pizza6,
     altText: 'Triple Box',
     caption: 'A Triple Box pizza is a hearty and flavorful option that packs a punch of taste in every bite. It typically features a generous amount of toppings, including multiple layers of cheese, meats, and vegetables. The base is often pre-baked to enhance its crispiness before adding a generous amount of tomato sauce. The combination of multiple toppings creates a rich, complex flavor profile that appeals to those who enjoy a robust pizza experience. Whether enjoyed as a family meal or a special treat, a Triple Box pizza is sure to satisfy cravings for a delicious and satisfying slice.',
-    key: 3,
+    key: 6,
   },
 ];
 
@@ -80,23 +80,16 @@ export function Offers(args) {
         className='CarouselItem'
         onExiting={() => setAnimating(true)}
         onExited={() => setAnimating(false)}
-        key={item.src}
+        key={item.key}
       >
+     
         <img className="ImageCarsoule ms-auto h-100 w-100 d-flex" src={item.src} alt={item.altText} />
+    
         <div className="Hover">
 
      <div className="HoverSystem">
-  
-     <div className="solar-system">
-      <div className="sun"></div>
-      <div className="planet"></div>
-      <div className="planet"></div>
-    </div>
 
      <h6>{item.caption}</h6>
-
-
-
      </div>
         </div>
       </CarouselItem>
@@ -120,13 +113,11 @@ export function Offers(args) {
 
       {slides}
       <CarouselControl
-        className=''
         direction="prev"
         directionText="Previous"
         onClickHandler={previous}
       />
       <CarouselControl
-        className=''
         direction="next"
         directionText="Next"
         onClickHandler={next}
